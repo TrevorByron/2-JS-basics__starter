1e357f253aa2a6be24f177c32854897be28d9fe6
TrevorByron <trevorborden@gmail.com  git config --global user.name Trevor>
2019-11-09 19:11:22 -0800

    The Ternary Operator and Switch Statements

--- script.js
+++ script.js
@@ -200,25 +200,66 @@ Boolean Logic
 */
 
 
-var firstName ='Jon';
-var age = 20;
-if (age < 13){
-    console.log(firstName + ' is a boy.');
-} else if (age >= 13 && age < 20) {
-    console.log(firstName + ' is a teenager.');
+//var firstName ='Jon';
+//var age = 20;
+//if (age < 13){
+//    console.log(firstName + ' is a boy.');
+//} else if (age >= 13 && age < 20) {
+//    console.log(firstName + ' is a teenager.');
+//}
+//else if ((age >= 20 && age < 30)){
+//    console.log(firstName + ' is a young man.');
+//}
+//else {
+//    console.log(firstName + ' is a man.');
+//}
+
+/****************************************************************************************************
+The Ternary Operator and Switch Statements
+*/
+//Ternary Operator
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+
+var drink = age >= 18 ? 'beer' : 'juice';
+
+console.log(drink);
+
+// Switch statement
+
+var job = 'instructor';
+switch (job) {
+    case 'teacher' :
+    case 'instructor':     
+        console.log (firstName + ' teaches kids how to code');
+        break;
+    case 'driver': 
+        console.log (firstName + ' drives an uber in lisbon');
+         break;
+     case 'driver': 
+        console.log (firstName + ' designs websites');
+         break;
+    default: 
+        console.log (firstName + ' does something else'); 
 }
-else if ((age >= 20 && age < 30)){
-    console.log(firstName + ' is a young man.');
+
+
+switch (true) {
+    case age < 13 :    
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20: 
+        console.log(firstName + ' is a teenager.');
+         break;
+     case age >= 20 && age < 30: 
+        console.log(firstName + ' is a young man.');
+         break;
+    default: 
+        console.log(firstName + ' is a man.'); 
 }
-else {
-    console.log(firstName + ' is a man.');
-}
-
-
-
-
-
-
 
 
 
