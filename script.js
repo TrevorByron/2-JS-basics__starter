e67cb82d45fab6348f3b4f5ec778c18e02bf78b6
TrevorByron <trevorborden@gmail.com  git config --global user.name Trevor>
2019-11-09 18:35:55 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,225 @@
+/****************************************************************************************************
+Variables and data types 
+*/
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable Naming Rules
+var _3years = 3;
+var johnMark = 'Jon And Mark'
+var if = 23;
+*/
+
+
+/****************************************************************************************************
+Variable Mutation and Type Coercion
+*/
+/*
+var firstName = 'John';
+var age = 28;
+
+//type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable Mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+/****************************************************************************************************
+Basic operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+// Math operators
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+
+// Logical operators 
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+
+
+/****************************************************************************************************
+Operator Precedence 
+*/
+
+/*
+var now =2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+//Grouping 
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) /2;
+
+console.log(average);
+
+//Mulitple Assignments 
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8
+console.log(x, y);
+
+// More operators
+
+
+x *= 2
+console.log(x);
+x += 10;
+console.log(x);
+x--;
+console.log(x);
+
+*/
+/****************************************************************************************************
+Coding Challenge 
+*/
+/*
+
+var markMass, johnMass, markHeight, johnHeight, markBMI, johnBMI;
+
+markMass = 78; // kg
+johnMass = 92; // kg
+
+markHeight = 1.69; // meters
+johnHeight = 1.95; // meters
+
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log('Mark has a BMI of ' + markBMI);
+console.log('John has a BMI of ' + johnBMI);
+
+
+var markMore = markBMI > johnBMI;
+console.log('Is Mark\'s BMI higher than John\s? ' + markMore);
+
+
+*/
+
+
+/****************************************************************************************************
+If Else Statments 
+*/
+
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married'){ 
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)')
+    
+}
+var isMarried ='true';
+if (isMarried){ 
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)')  
+}
+
+
+var markMass, johnMass, markHeight, johnHeight, markBMI, johnBMI;
+
+markMass = 78; // kg
+johnMass = 92; // kg
+
+markHeight = 1.69; // meters
+johnHeight = 1.95; // meters
+
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+if(markBMI > johnBMI) {
+    console.log('Mark\'s BMI is higher than John\s? ')
+} else {
+    console.log('John\'s BMI is higher than Mark\s? ')
+}
+
+//console.log('Mark has a BMI of ' + markBMI);
+//console.log('John has a BMI of ' + johnBMI);
+
+
+//var markMore = markBMI > johnBMI;
+//console.log('Is Mark\'s BMI higher than John\s? ' + markMore);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
